789fc5551072f7a010826c9388f5c4c346b75a53
Calvin Mann <calvinmann@gmail.com>
2019-05-23 22:14:57 +0200

    Added new routine for merging schemas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 module.exports = {
     graph: require('./graph.js'),
     edge: require('./edge.js'),
+    util: require('./util.js'),
     protect: require('./protect.js')
 };
---
+++ src/util.js
@@ -0,0 +1,50 @@
+module.exports = { merge };
+
+const { ObjectTypeComposer } = require('graphql-compose');
+
+function merge(destination, source) {
+    for (const [name, composer] of source.entries()) {
+        if (composer instanceof ObjectTypeComposer) {
+            if (!destination.has(name)) {
+                const sourceFields = composer.getFields();
+
+                const otc = destination.createObjectTC({
+                    name,
+                    interfaces: composer.getInterfaces().map(typeName),
+                    extensions: composer.getExtensions(),
+                    description: composer.getDescription(),
+                    fields: Object.keys(sourceFields).reduce(cloneField, {})
+                });
+
+                for (const [resolverName, resolver] in composer.getResolvers()) {
+                    otc.setResolver(resolverName, resolver);
+                }
+            }
+        }
+    }
+
+    function cloneField(result, name) {
+        const sourceField = source.getField(name);
+        result[name] = {
+            type: typeName(sourceField.type),
+            args: Object.keys(sourceField.args).reduce((result, argName) => {
+                result[argName] = typeName(sourceField.args[argName]);
+                return result;
+            }, {}),
+            resolve: sourceField.resolve,
+            subscribe: sourceField.subscribe,
+            deprecationReason: sourceField.deprecationReason,
+            description: sourceField.description,
+            extensions: sourceField.extensions
+        };
+        return result;
+    }
+
+    function typeName(type) {
+        if (typeof type === 'string') {
+            return type;
+        } else {
+            return type.getTypeName();
+        }
+    }
+}
