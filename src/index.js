9e2564464abaf44ed5ce72d1f77b65378aa5f566
Calvin Mann <calvinmann@gmail.com>
2019-06-01 17:24:17 +0200

    Removed combine module (This has been moved to @major-mann/graphql-mutator)

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 module.exports = {
     edge: require('./edge.js'),
     util: require('./util.js'),
-    protect: require('./protect.js'),
-    combine: require('./combine.js')
+    protect: require('./protect.js')
 };
